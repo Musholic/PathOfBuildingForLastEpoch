9659cdf520c33267d25d4b51afcc7d3469824a64
Musholic <musholic.tbg@gmail.com>
2024-03-21 15:34:00 +0100

    Support for idols and multiAffixes

--- gameFileExtractScript/processMods.py
+++ gameFileExtractScript/processMods.py
@@ -3,6 +3,20 @@ import json
 import re
 from natsort import natsorted
 
+
+def get_value_range(tier_data):
+    min_roll = tier_data["minRoll"]
+    max_roll = tier_data["maxRoll"]
+    is_percentage = isinstance(min_roll, float)
+    if is_percentage:
+        min_roll = int(100 * min_roll)
+        max_roll = int(100 * max_roll)
+    value_range = "+(" + str(min_roll) + "-" + str(max_roll) + ")"
+    if is_percentage:
+        value_range += "%"
+    return value_range
+
+
 # Implicit properties
 with open("originalAssets/MasterPropertyList.asset", "r") as yamlFile:
     data = yaml.safe_load(yamlFile)["MonoBehaviour"]
@@ -36,28 +50,40 @@ for affixStringData in affixStringsData:
     affixStringsById[affixStringData["m_Id"]] = affixStringData["m_Localized"]
 affixStrings = {}
 for affixKeyData in affixKeysData:
-    match = re.search(r'Item_Affix_(\d+)_DisplayName', affixKeyData["m_Key"])
+    match = re.search(r'Item_?Affix_(\d+)_Affix_([AB])', affixKeyData["m_Key"])
     if match:
         affixId = match.group(1)
-        affixStrings[affixId] = affixStringsById[affixKeyData["m_Id"]]
+        affixAB = match.group(2)
+        affixStrings[affixId + "_" + affixAB] = affixStringsById[affixKeyData["m_Id"]]
 
 modList = {}
 
 for affixData in data["singleAffixes"]:
     for tier, tierData in enumerate(affixData["tiers"]):
-        minRoll = tierData["minRoll"]
-        maxRoll = tierData["maxRoll"]
-        isPercentage = isinstance(minRoll, float)
-        if isPercentage:
-            minRoll = int(100 * minRoll)
-            maxRoll = int(100 * maxRoll)
-        valueRange = "+(" + str(minRoll) + "-" + str(maxRoll) + ")"
-        if isPercentage:
-            valueRange += "%"
-        affixName = affixStrings[str(affixData["affixId"])]
+        valueRange = get_value_range(tierData)
+        affixName = affixStrings[str(affixData["affixId"]) + "_A"]
         modData = {
             "affix": affixData["affixTitle"],
             "value": valueRange + " " + affixName,
+            "level": affixData["levelRequirement"]
+        }
+        if affixData["rollsOn"]:
+            modData["type"] = "Prefix"
+        else:
+            modData["type"] = "Suffix"
+        modList[str(affixData["affixId"]) + "_" + str(tier)] = modData
+
+for affixData in data["multiAffixes"]:
+    for tier, tierData in enumerate(affixData["tiers"]):
+        valueRange = get_value_range(tierData)
+        affixName = affixStrings[str(affixData["affixId"]) + "_A"]
+        value = valueRange + " " + affixName + "\n"
+        valueRange = get_value_range(tierData["extraRolls"][0])
+        affixName = affixStrings[str(affixData["affixId"]) + "_B"]
+        value += valueRange + " " + affixName
+        modData = {
+            "affix": affixData["affixTitle"],
+            "value": value,
             "level": affixData["levelRequirement"]
         }
         if affixData["rollsOn"]:
