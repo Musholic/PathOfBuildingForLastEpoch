fb06dcd7dfb737d32019e984857b081ad1ed39bd
Musholic <musholic.tbg@gmail.com>
2024-03-29 14:42:52 +0100

    Fix LE Tools import for items

---
+++ gameFileExtractScript/extractLEToolsImportData.js
@@ -0,0 +1,2 @@
+JSON.stringify(Object.fromEntries(Object.entries(le_k).map(([k, v]) => [k, le_a(v.displayNameKey)])))
+JSON.stringify(Object.fromEntries(Object.entries(le_l).concat(Object.entries(le_m)).map(([k, v]) => [k, v.affixId])))
