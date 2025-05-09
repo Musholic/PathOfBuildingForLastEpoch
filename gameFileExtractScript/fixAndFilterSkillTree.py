87014b0ac66c03f15353e57f767c6aca878fcc35
Musholic <musholic.tbg@gmail.com>
2025-05-09 10:17:52 +0100

    Convert new game file extract scripts to a melon loader mod

--- gameFileExtractScript/fixAndFilterSkillTree.py
+++
@@ -1,8 +0,0 @@
-from common import *
-
-
-# Read the YAML file
-dataNodes = []
-fix_and_filter_yaml_file(prefabPath + "Skill Trees.prefab", "generatedAssets/skillTrees.yaml")
-
-print("skillTrees.yaml generated with success")
--- gameFileExtractScript/fixAndFilterTree.py
+++
@@ -1,8 +0,0 @@
-from common import *
-
-
-# Read the YAML file
-dataNodes = []
-fix_and_filter_yaml_file(prefabPath + "PassiveTree2021.prefab", "generatedAssets/passiveTree.yaml")
-
-print("passiveTree.yaml generated with success")
