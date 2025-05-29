44a63ea5f28db9bf89d70a41000ce187771aa869
Musholic <musholic.tbg@gmail.com>
2025-05-29 21:18:10 +0200

    Update LETools import data

---
+++ gameFileExtractScript/extractLEToolsImportData.js
@@ -0,0 +1,7 @@
+// https://www.lastepochtools.com/profile/js/script.js
+function le_e(a) {
+    return null != a.displayNameKey ? (a = le_c(a.displayNameKey),
+    le_wa(a)) : a.displayName
+}
+JSON.stringify(Object.fromEntries(Object.entries(le_k).map(([k, v]) => [k, le_e(v)])))
+JSON.stringify(Object.fromEntries(Object.entries(le_l).concat(Object.entries(le_m)).map(([k, v]) => [k, v.affixId])))
